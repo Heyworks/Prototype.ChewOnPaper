93583f16561697f747dfdfe9de6316cf2f93c6db
Artur Sushkov <a.sushkov@heyworks.com>
2017-07-05 17:49:18 +0300

    Add some main screen code.

---
+++ ChewOnPaper/Assets/Scripts/Model/RoomSettings.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Describe all game room settings
+/// </summary>
+public class RoomSettings
+{
+    /// <summary>
+    /// Gets or sets maximum count of players in the room.
+    /// </summary>
+    public int MaxPlayers{get;set;}
+
+    /// <summary>
+    /// Gets or sets time for player's turn.
+    /// </summary>
+    public int TurnTime{get;set;}
+
+    /// <summary>
+    /// Gets or sets score bonus for right answer.
+    /// </summary>
+    public int RightAnswerScore{get;set;}
+
+    /// <summary>
+    /// Gets or sets score bonus for last turn. 
+    /// </summary>
+    public int LastTurnScore { get; set; }
+}
---
+++ ChewOnPaper/Assets/Scripts/UI/Screens/MainScreen/MainScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Represents main screen for room setup and select.
+/// </summary>
+public class MainScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Button test;
+
+    [SerializeField] private SettingsView settingsView;
+
+    private void Start()
+    {
+        test.onClick.AddListener(OnClick);
+        var defSettings = new RoomSettings();
+        defSettings.MaxPlayers = 4;
+        settingsView.WriteSettings(defSettings);
+    }
+
+    private void OnClick()
+    {
+        var settings = settingsView.ReadSettings();
+        Debug.Log("settings.MaxPlayers " + settings.MaxPlayers);
+    }
+}
