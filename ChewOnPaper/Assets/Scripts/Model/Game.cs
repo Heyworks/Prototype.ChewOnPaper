efe3cca019a4cdc9708705894fbb40bb1de96708
Artur Sushkov <a.sushkov@heyworks.com>
2017-07-14 11:41:38 +0300

    Add Game model.

---
+++ ChewOnPaper/Assets/Scripts/Model/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Represents Game model presentation.
+/// </summary>
+public class Game
+{
+    /// <summary>
+    /// Gets the current player identifier.
+    /// </summary>
+    public string CurrentPlayerId { get; private set; }
+
+    /// <summary>
+    /// Gets the players.
+    /// </summary>
+    public List<Player> Players { get; private set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Game"/> class.
+    /// </summary>
+    /// <param name="currentPlayerId">The current player identifier.</param>
+    /// <param name="players">The players.</param>
+    public Game(string currentPlayerId, List<Player> players)
+    {
+        CurrentPlayerId = currentPlayerId;
+        Players = players;
+    }
+}
---
+++ ChewOnPaper/Assets/Scripts/Model/Player.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// Represents player model presentation.
+/// </summary>
+public class Player
+{
+    /// <summary>
+    /// Gets the identifier.
+    /// </summary>
+    public string Id { get; private set; }
+
+    /// <summary>
+    /// Gets the name.
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the session role.
+    /// </summary>
+    public PlayerRole Role { get; set; }
+
+    /// <summary>
+    /// Gets or sets the score.
+    /// </summary>
+    public int Score { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Player"/> class.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <param name="name">The name.</param>
+    public Player(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
