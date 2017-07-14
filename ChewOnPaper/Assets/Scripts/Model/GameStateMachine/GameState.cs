b9fe9008bdd4cbb98f3e4f82f7c5202f5ae41fe0
Artur Sushkov <a.sushkov@heyworks.com>
2017-07-14 12:36:47 +0300

    Add state machine base.

---
+++ ChewOnPaper/Assets/Scripts/Model/GameStateMachine/GameState.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Represents base class for all game states.
+/// </summary>
+public abstract class GameState
+{
+    /// <summary>
+    /// Gets a value indicating whether this state is active.
+    /// </summary>
+    protected bool IsActive { get; private set; }
+
+    /// <summary>
+    /// Acticates this state.
+    /// </summary>
+    public void Acticate()
+    {
+        IsActive = true;
+    }
+
+    /// <summary>
+    /// Deactivates this State.
+    /// </summary>
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+}
---
+++ ChewOnPaper/Assets/Scripts/Model/GameStateMachine/GameStateMachine.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// Represents game state machine.
+/// </summary>
+public class GameStateMachine
+{
+    private GameState currentState;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameStateMachine"/> class.
+    /// </summary>
+    public GameStateMachine()
+    {
+        CreateStates();
+    }
+
+    /// <summary>
+    /// Starts state machine work.
+    /// </summary>
+    public void Start()
+    {
+        currentState.Acticate();
+    }
+
+    private void CreateStates()
+    {
+        
+    }
+}
