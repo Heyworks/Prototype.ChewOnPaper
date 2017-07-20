4d18f3601f10dc2c9644cfea3877c925c887051c
Artur Sushkov <a.sushkov@heyworks.com>
2017-07-20 15:52:50 +0300

    Add Leaderboard view.

---
+++ ChewOnPaper/Assets/Scripts/UI/LeaderboardView.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+/// <summary>
+/// Represents leaderboard view.
+/// </summary>
+public class LeaderboardView : MonoBehaviour
+{
+    [Inject]
+    private Game game;
+
+    [SerializeField]
+    private Text leaderboardText;
+
+    /// <summary>
+    /// Updates the leaderboard.
+    /// </summary>
+    public void UpdateLeaderboard()
+    {
+        var players = game.Players.OrderByDescending(item => item.Score);
+
+        leaderboardText.text = string.Empty;
+
+        foreach (var player in players)
+        {
+            leaderboardText.text += string.Format("{0} : {1} \n", player.Name, player.Score);
+        }
+    }
+}
